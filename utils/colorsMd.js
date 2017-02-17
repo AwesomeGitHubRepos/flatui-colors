2b9e2074b843eea673d4002d9cca8a370aea7f1e
guanglei.xiegl <guanglei.xiegl@alibaba-inc.com>
2017-02-17 18:45:55 +0800

    fix readme

--- utils/colorsMd.js
+++ utils/colorsMd.js
@@ -5,7 +5,7 @@ var str = '';
 for(var key in colors) {
   if (colors.hasOwnProperty(key)) {
     if (key !== key.toUpperCase()) {
-      str += '* <span style="font-weight: bolder;">' + key + '</span>&nbsp;<span style="background-color: ' + colors[key] + '; display: inline-block;">&nbsp;&nbsp;&nbsp;<span>\n'
+      str += '* <span style="font-weight: bolder;">' + key + '</span>&nbsp;<span style="background-color: ' + colors[key] + '; display: inline-block;">&nbsp;&nbsp;&nbsp;</span>\n';
     }
   }
 }
